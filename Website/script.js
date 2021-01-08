70f690c32b87410dfc80babdb51e5ad4466639cd
Mohammad-Fayyaz-Kirmani <kirmani92110@gmail.com>
2021-01-09 00:45:57 +0500

    my site

---
+++ Website/script.js
@@ -0,0 +1,8 @@
+function Down(el) {
+    var elem = document.getElementById(el);
+    elem.style.transition = "all 2s ease-in-out";
+    elem.style.height = "400px";
+    elem.style.padding = "14px";
+    elem.style.paddingLeft ="30px";
+    elem.style.paddingTop = "3%"
+  }
